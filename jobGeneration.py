4b8fa89c7b086fae1b00b8b2e167165de51a57d3
YitaoLiang <yliang@fandm.edu>
2015-07-02 09:54:45 -0400

    Delete jobGeneration.py

--- jobGeneration.py
+++
@@ -1,9 +0,0 @@
-def main():
-	BproComparision = open('BproJobs.txt','a')
-	for i in range(1,31):
-		line = 'Bpro-Pong-Trial'+str(i)+'\t./learnerBpro -s '+str(i)+' -c bpro.cfg -r ../roms/pong.bin -n Bpro-Pong-Trial'+str(i)+'\n'
-		BproComparision.write(line)
-	BproComparision.close()
-
-
-main()
